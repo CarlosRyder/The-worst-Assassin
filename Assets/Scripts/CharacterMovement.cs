d893c325aa98988b058bdfa8d5aca895dfed1b9d
Carlos Palacios <carlos_a.riascos@uao.edu.co>
2024-07-10 00:41:02 -0500

    Player movement animation

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,15 +7,17 @@ public class CharacterMovement : MonoBehaviour
     private Rigidbody playerRigidbody;
     private float movX, movZ;
     public float degrees = 2;
-    public float speed = 4;
+    public float speed = 8;
     public float jumpForce = 6;
-    public float fallMultiplier = 1; 
+    public float fallMultiplier = 1;
     private bool isJumping;
     private Vector3 movement;
+    public Animator animator;
 
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -23,6 +25,13 @@ public class CharacterMovement : MonoBehaviour
         movX = Input.GetAxisRaw("Horizontal");
         movZ = Input.GetAxisRaw("Vertical");
         movement = new Vector3(movX, 0, movZ).normalized;
+
+        // Update animator parameters
+        animator.SetFloat("Speed", movement.magnitude);
+        animator.SetBool("IsJumping", isJumping);
+        animator.SetFloat("MovementX", movX);
+        animator.SetFloat("MovementZ", movZ);
+
         Jump();
     }
 
@@ -37,39 +46,52 @@ public class CharacterMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isJumping = false;
+            animator.SetBool("IsJumping", isJumping);
         }
     }
 
-    private void Movement() 
+    private void Movement()
     {
         if (movement.magnitude > 0)
         {
-            
             Vector3 move = transform.position + movement * Time.deltaTime * speed;
             playerRigidbody.MovePosition(move);
-        
-
-            //Quaternion targetRotation = Quaternion.LookRotation(movement, Vector3.up);
-            //playerRigidbody.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, degrees * Time.deltaTime));
+        }
+        if (movX > 0) // Moving right
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        else if (movX < 0) // Moving left
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        else if (movZ > 0) // Moving forward
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (movZ < 0) // Moving backward
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
         }
         else
         {
-            //stop movement
+            // Stop Movement
             playerRigidbody.velocity = new Vector3(0, playerRigidbody.velocity.y, 0);
-            playerRigidbody.angularVelocity = Vector3.zero; 
+            playerRigidbody.angularVelocity = Vector3.zero;
         }
     }
 
-    private void Jump() 
+    private void Jump()
     {
         if (Input.GetButtonDown("Jump") && !isJumping)
         {
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isJumping = true;
+            animator.SetBool("IsJumping", isJumping);
         }
     }
 
-    private void JumpFall() 
+    private void JumpFall()
     {
         if (playerRigidbody.velocity.y < 0)
         {
